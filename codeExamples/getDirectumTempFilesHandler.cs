a812a591bad57d040c4f1324c4943ba6eea3488e
NatalyRaspberryJam <nataly.xomka@gmail.com>
2015-08-13 22:35:44 +0300

    code examples

---
+++ codeExamples/getDirectumTempFilesHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SBRte;
+using SBLogon;
+using Tenders.ui;
+using System.Threading;
+
+namespace Tenders.handlers
+{
+  static public class getDirectumTempFilesHandler
+  {
+    static public void getDirectumFiles()
+    {
+      string temp = createTempFolder();
+        getFilesFromDirectum(temp);      
+          parseDocxHandler.setDocFolder(temp);
+          deleteTempFolder(temp);   
+    }
+
+    static private void getFilesFromDirectum(string temp)
+    {
+ 	    var connectionParams = "SystemCode=DIRECTUM";
+      SBLogon.LoginPoint logP = new SBLogon.LoginPoint();
+      SBRte.Application dirApp = logP.GetApplication(connectionParams);
+        IScriptFactory scriptFactory = dirApp.ScriptFactory;
+        SBRte.IList directumDocs = scriptFactory.ExecuteByName("_getSpecialDocuments");  //вызов сценария получения файлов
+      IEDocumentFactory docFactory = dirApp.EDocumentFactory;
+
+      while (directumDocs.EOF == false)
+      {
+        //последняя версия дока
+        var doc = docFactory.GetObjectByID(directumDocs.Value.ID);
+        var versions = doc.versions;
+        int versionNum = versions.Count;
+          //куда сохранить
+          string fileName = docFactory.GetObjectByID(directumDocs.Value.ID).Requisites("ISBEDocName").DisplayText;
+          string saveFileDirectory = temp + "\\" + fileName + ".docx";
+          docFactory.GetObjectByID(directumDocs.Value.ID).Export(versionNum, @saveFileDirectory, false, false, false, TExportedSignaturesType.estAll);
+        directumDocs.Next();
+      }
+    }
+
+    static private string createTempFolder()  //temp folder
+    {
+      string userDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+      string tempFolder = Path.Combine(userDirectory, "tempDirectumFolder");
+        Directory.CreateDirectory(tempFolder);
+      return tempFolder;
+    }
+
+    static private void deleteTempFolder(string folder)
+    {
+      Directory.Delete(folder, true);
+    }
+
+  }
+}
---
+++ codeExamples/parseDocxHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Novacode;
+using Tenders.handlers;
+
+namespace Tenders.handlers
+{
+  static public class parseDocxHandler
+  {
+    static public void setDocFolder(string folder)
+    {
+      string[] files = Directory.GetFiles(folder);
+        foreach (string path in files)
+        {
+          DocX file = DocX.Load(@path);
+          parseFile(file);
+        }
+    }
+
+    static private void parseFile(DocX file)
+    {
+      string initiator = file.CustomProperties.Where(x => x.Value.Name == "ФИО инициатора").First().Value.Value.ToString();
+      string phone = file.CustomProperties.Where(x => x.Value.Name == "Телефон инициатора").First().Value.Value.ToString();
+      string date = file.CustomProperties.Where(x => x.Value.Name == "Текущая дата").First().Value.Value.ToString();
+      string bankroll = file.CustomProperties.Where(x => x.Value.Name == "Средства").First().Value.Value.ToString();
+      string department = file.CustomProperties.Where(x => x.Value.Name == "Р*Подразделение...*Наименование").First().Value.Value.ToString();
+      string content = file.CustomProperties.Where(x => x.Value.Name == "Аннотация").First().Value.Value.ToString();
+      string financing = file.CustomProperties.Where(x => x.Value.Name == "Финансирование").First().Value.Value.ToString();
+
+        List<item> items = new List<item>();
+          var equipmentTable = file.Tables[0];
+            for (int i = 1; i < equipmentTable.Rows.Count; i++)
+            {
+              items.Add(manageNotesHandler.createDirItem(equipmentTable.Rows[i].Cells[0].Paragraphs.First().Text, Convert.ToInt32(equipmentTable.Rows[i].Cells[1].Paragraphs.First().Text), equipmentTable.Rows[i].Cells[2].Paragraphs.First().Text));
+            }
+        manageNotesHandler.addDirNote(content, department, initiator, phone, date, bankroll, financing, items);      
+    }
+  }
+}
