811fa56fcd8a2bb71d63b84b5917e4f9bd7287f7
NatalyRaspberryJam <nataly.xomka@gmail.com>
2015-08-13 22:32:04 +0300

    project

---
+++ triangleApp/testProject/unitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using triangleApp;
+
+namespace testProject
+{
+  [TestClass]
+  public class unitTest
+  {
+    [TestMethod]
+    public void testIntegers()
+    {
+      triangle t = new triangle("3", "4", "5");
+        Assert.AreEqual(6, t.getTriangleSquare());
+    }
+
+    [TestMethod]
+    public void testNegativeInt()
+    {
+      triangle t = new triangle("-3", "-4", "-5");
+        Assert.AreEqual(6, t.getTriangleSquare());
+    }
+
+    [TestMethod]
+    public void testStrings()
+    {
+      triangle t = new triangle("2a", "10", "re");
+        Assert.AreEqual(0, t.getTriangleSquare());
+    }
+
+    [TestMethod]
+    public void testDouble()
+    {
+      triangle t = new triangle("4.35", "7", "10");
+        Assert.AreEqual(0, t.getTriangleSquare());
+    }
+
+  }
+}
---
+++ triangleApp/triangleApp/triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangleApp
+{
+  public class triangle
+  {
+    int side1;
+    int side2;
+    int side3;
+
+    public triangle(string first, string second, string third)
+    {
+      try
+      {
+        side1 = int.Parse(first);
+        side2 = int.Parse(second);
+        side3 = int.Parse(third);
+      }
+      catch { }
+    }
+
+    public double getTriangleSquare()  //площадь треугольника, принимает на вход длину каждой из сторон
+    {
+      var prmtr = (side1 + side2 + side3) / 2;
+      return Math.Sqrt(prmtr * (prmtr - side1) * (prmtr - side2) * (prmtr - side3));
+    }
+  }
+}
